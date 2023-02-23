ec5874315f041d990cae03c4f1da736ab1fb3572
Felipe Barros <48369466+FelipeJBarros@users.noreply.github.com>
2023-02-23 11:15:02 -0300

    ADD: Product data validation

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -3,6 +3,7 @@ import 'express-async-errors';
 
 import express, { Request, Response, NextFunction } from 'express';
 import cors from 'cors';
+import { errors } from 'celebrate';
 import routes from './routes';
 import AppError from '@shared/errors/AppErrors';
 import '@shared/typeorm';
@@ -13,6 +14,8 @@ app.use(cors());
 app.use(express.json());
 
 app.use(routes);
+
+app.use(errors());
 
 app.use(
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
