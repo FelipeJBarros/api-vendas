15d52905a70c1317511386cafa694d3f1e6dcea0
Felipe J Barros <felipebarros.engh@gmail.com>
2024-01-17 15:51:29 -0300

    fix: corrigindo a implementação do typeORM em sua nova versão
    A partir da versão 0.3.x o typeORM teve uma BREAKING CHANGE, fazendo com que a forma de conectar o
    bando de dados mudasse bastante, portanto foram feitas mudanças para realizar a conexão da forma
    mais moderna indicada em documentação

---
+++ src/shared/http/app.ts
@@ -0,0 +1,35 @@
+import express, { NextFunction, Request, Response } from 'express';
+import cors from 'cors';
+import routes from './routes';
+import AppError from '@shared/errors/AppError';
+import '@shared/typeorm';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use(routes);
+
+// Middleware de tratamento de erro personalizado
+app.use(
+    (
+        error: Error,
+        request: Request,
+        response: Response,
+        next: NextFunction,
+    ) => {
+        if (error instanceof AppError) {
+            return response.status(error.statusCode).json({
+                status: error.statusCode,
+                message: error.message,
+            });
+        }
+        return response.status(500).json({
+            status: 500,
+            message: 'Internal server error',
+        });
+    },
+);
+
+export { app };
--- src/shared/typeorm/index.ts
+++ src/shared/typeorm/index.ts
@@ -1,3 +1,10 @@
-import { createConnection } from 'typeorm';
+import { DataSource } from 'typeorm';
 
-createConnection();
+export const dataSource = new DataSource({
+    type: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    username: 'postgres',
+    password: 'docker',
+    database: 'apivendas'
+});
