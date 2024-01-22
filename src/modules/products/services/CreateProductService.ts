87484dfc95fdc9a848a832ef0a2cc519a0258bf7
Felipe J Barros <felipebarros.engh@gmail.com>
2024-01-22 15:46:38 -0300

    feat: ajustes no repositório de produtos e criação do primeiro serviço de produtos
    Foi adicionado o primeiro serviço do módulo de produtos, referente a criação de novos produtos. Além
    disso, foram feitos ajustes no repositório para suportar a implementação do serviço em questão

---
+++ src/modules/products/services/CreateProductService.ts
@@ -0,0 +1,29 @@
+import AppError from '@shared/errors/AppError';
+import { ProductRepository } from '../typeorm/repositories/ProductsRepository';
+import { ICreateProduct } from '../models/registerProductModel';
+import Product from '../typeorm/entities/Product';
+
+class CreateProductService {
+    public async execute({
+        name,
+        price,
+        quantity,
+    }: ICreateProduct): Promise<Product> {
+        const productsRepository = new ProductRepository();
+        const productExists = await productsRepository.findByName(name);
+
+        if (productExists) {
+            throw new AppError('Já existe um produto cadastrado com esse nome');
+        }
+
+        const newProduct = await productsRepository.create({
+            name,
+            price,
+            quantity,
+        });
+
+        return newProduct;
+    }
+}
+
+export default CreateProductService;
--- src/modules/products/typeorm/repositories/ProductsRepository.ts
+++ src/modules/products/typeorm/repositories/ProductsRepository.ts
@@ -1,12 +1,19 @@
 import { dataSource } from '@shared/typeorm';
 import { Repository } from 'typeorm';
 import Product from '../entities/Product';
+import { ICreateProduct } from '@modules/products/models/registerProductModel';
 
 export class ProductRepository {
     private ormRepository: Repository<Product>;
 
     constructor() {
         this.ormRepository = dataSource.getRepository(Product);
+    }
+
+    public async create({ name, price, quantity }: ICreateProduct) {
+        const newProduct = this.ormRepository.create({ name, price, quantity });
+        await this.ormRepository.save(newProduct);
+        return newProduct;
     }
 
     public async findByName(name: string): Promise<Product | null> {
