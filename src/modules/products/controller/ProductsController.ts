26e1fc49a2144b9073236e5c22796230fac4b7ce
Felipe J Barros <felipebarros.engh@gmail.com>
2024-01-23 14:51:47 -0300

    feat(produto): add método de listagem no controller de produtos

--- src/modules/products/controller/ProductsController.ts
+++ src/modules/products/controller/ProductsController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import CreateProductService from '../services/CreateProductService';
+import ListProductService from '../services/ListProductService';
 
 class ProductsController {
     public async create(
@@ -16,6 +17,18 @@ class ProductsController {
         });
 
         return response.json(newProduct);
+    }
+
+    public async list(request: Request, response: Response): Promise<Response> {
+        const { page, limit } = request.query;
+        const listProductService = new ListProductService();
+
+        const listProducts = await listProductService.execute(
+            Number(page) || 0,
+            Number(limit) || 10,
+        );
+
+        return response.json(listProducts);
     }
 }
 
