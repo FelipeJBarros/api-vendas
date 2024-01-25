86c9791c0613a0cabfccfc740e760d2337e508f5
Felipe J Barros <felipebarros.engh@gmail.com>
2024-01-25 14:37:09 -0300

    feat(route): adicionado a validação dos parâmetros de rotas de produtos
    Usando a biblioteca celebrate estamos validando os parâmetros que são enviados para as rotas de
    produtos. A partir dessa validação lidamos com os erros gerados pelo não envio de parâmetros
    obrigatórios e/ou parâmetros enviados em formatos diferentes do esperado

--- src/modules/products/routes/products.routes.ts
+++ src/modules/products/routes/products.routes.ts
@@ -1,13 +1,57 @@
 import { Router } from 'express';
+import { celebrate, Joi, Segments } from 'celebrate';
 import ProductsController from '../controller/ProductsController';
 
 const productsRouter = Router();
 const productsController = new ProductsController();
 
 productsRouter.get('/', productsController.list);
-productsRouter.get('/:id', productsController.index);
-productsRouter.post('/', productsController.create);
-productsRouter.put('/:id', productsController.update);
-productsRouter.delete('/:id', productsController.delete);
+
+productsRouter.get(
+    '/:id',
+    celebrate({
+        [Segments.PARAMS]: Joi.object().keys({
+            id: Joi.string().uuid().required(),
+        }),
+    }),
+    productsController.index,
+);
+
+productsRouter.post(
+    '/',
+    celebrate({
+        [Segments.BODY]: Joi.object().keys({
+            name: Joi.string().required(),
+            price: Joi.number().precision(2).required(),
+            quantity: Joi.number().required(),
+        }),
+    }),
+    productsController.create,
+);
+
+productsRouter.put(
+    '/:id',
+    celebrate({
+        [Segments.PARAMS]: Joi.object().keys({
+            id: Joi.string().uuid().required(),
+        }),
+        [Segments.BODY]: Joi.object().keys({
+            name: Joi.string().required(),
+            price: Joi.number().precision(2).required(),
+            quantity: Joi.number().required(),
+        }),
+    }),
+    productsController.update,
+);
+
+productsRouter.delete(
+    '/:id',
+    celebrate({
+        [Segments.PARAMS]: Joi.object().keys({
+            id: Joi.string().uuid().required(),
+        }),
+    }),
+    productsController.delete,
+);
 
 export default productsRouter;
--- src/shared/http/app.ts
+++ src/shared/http/app.ts
@@ -4,6 +4,7 @@ import cors from 'cors';
 import routes from './routes';
 import AppError from '@shared/errors/AppError';
 import '@shared/typeorm';
+import { errors } from 'celebrate';
 
 const app = express();
 
@@ -11,6 +12,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use(routes);
+app.use(errors());
 
 // Middleware de tratamento de erro personalizado
 app.use(
